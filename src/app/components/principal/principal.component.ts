0b6799eb9f984e0f2b54b992fb102b5914680521
Thales <thales.navasques@gmail.com>
2023-05-31 17:52:45 -0300

    :sparkles: feat: Criação da função listar()/principal.component.ts

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from 'src/app/model/Cliente';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -8,4 +10,12 @@ import { Component } from '@angular/core';
 export class PrincipalComponent {
 
   btnCadastro: boolean = true;
+
+  constructor(private servico: ClienteService) {  }
+
+  clientes: Cliente[] =  [];
+
+  listar():void {
+    this.servico.listar().subscribe(retorno => this.clientes = retorno);
+  }
 }
