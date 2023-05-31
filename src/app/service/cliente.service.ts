fafee0a5f2a39d8281b6842b72449a1524757a2c
Thales <thales.navasques@gmail.com>
2023-05-31 17:32:47 -0300

    :sparkles: feat: Inclusão da variavel http para conectar na API

--- src/app/service/cliente.service.ts
+++ src/app/service/cliente.service.ts
@@ -1,9 +1,13 @@
+
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  private url: string = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
 }
